f72d624929e09975ae2eeb8be6ae36ea5a60a7aa
Sharik <sharikkarimi08@gmail.com>
2026-01-26 04:04:58 -0800

    com

--- src/methodoverloading/MethodOverlo.java
+++ src/methodoverloading/MethodOverlo.java
@@ -3,7 +3,7 @@ package methodoverloading;
 class LoginService{
 
     public String login(String username, String password){
-        return "Login using username and password";
+        return "Login using username and password"+password;
     }
 
     public void login(String username, int otp){
@@ -20,6 +20,8 @@ public class MethodOverlo {
         String s = lg.login("samMathur","jedjhjbduknd");
 
         System.out.println(s);
+        System.out.println("Hello from local");
+
 
         lg.login("michealDubey",1234);
 
